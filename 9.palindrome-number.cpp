ffeb9710177a5a61133c8a2f1eae49bb1a4c904c
RuiJCS <rjcsvv@gmail.com>
2022-06-16 17:16:45 +0100

    Better solution for exercise 9
    Being frustated with the results i was having I decided I'd try to find
    a better solution and decided to reverse the process i was doing instead
    of saving the values in an array.
    
    Accepted
    11510/11510 cases passed (18 ms)
    Your runtime beats 62.59 % of cpp submissions
    Your memory usage beats 74.02 % of cpp submissions (5.9 MB)

--- 9.palindrome-number.cpp
+++ 9.palindrome-number.cpp
@@ -10,31 +10,15 @@ class Solution
 public:
     bool isPalindrome(int x)
     {
-        bool res = true;
-        if (x > 0)
+        int initial_x = x;
+        uint32_t x_aux = 0;
+        while (x > 0)
         {
-            int N = log10(x);
-            int *digits = new int[N / 2 + 1];
-
-            for (size_t i = 0; i < N + 1; i++)
-            {
-                int aux = x % 10;
-                if (i <= N / 2)
-                    digits[i] = aux;
-                else if (aux != digits[N - i])
-                {
-                    res = false;
-                }
-                x = x / 10;
-            }
-
-            delete[] digits;
+            x_aux = x_aux * 10 + x % 10;
+            x = x / 10;
         }
-        else if (x < 0)
-        {
-            res = false;
-        }
-        return res;
+
+        return x_aux == initial_x;
     }
 };
 // @lc code=end
